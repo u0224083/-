23173f2c10f05b78de28f83fd0ed2d3f554b9667
u0224083 <u0224083@mis.nkfust.edu.tw>
2015-10-04 22:30:40 +0800

    送出JSON檔給connectDB.js

---
+++ db.php
@@ -0,0 +1,12 @@
+<?php //接JSON資料
+include "./connection.php";
+$query = "SELECT * FROM book" or die(mysql_error());
+$result = mysql_query($query);
+$testArr = array();
+
+while ($data = mysql_fetch_assoc($result)){
+    $testArr[]=$data;
+}
+echo json_encode($testArr);
+
+?>
